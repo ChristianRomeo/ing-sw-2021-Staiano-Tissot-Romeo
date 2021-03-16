ba2f79f90fd026a1a57de68fe20d610c8927226b
Christian Romeo <72473741+ilchris21@users.noreply.github.com>
2021-03-16 13:01:55 +0100

    AppTest

---
+++ src/main/java/it.polimi.ingsw/pRODUCT.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw;
+
+public class pRODUCT {
+
+    private String name="no name";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("hello");
+    }
+}
