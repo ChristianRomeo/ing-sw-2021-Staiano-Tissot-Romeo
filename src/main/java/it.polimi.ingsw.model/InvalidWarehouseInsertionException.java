cccb1afbeaf1641844369c37f92ce51d5a269d21
Tommaso Tissot <tissot.tommaso@outlook.it>
2021-03-28 21:40:24 +0200

    Added some Javadoc + Refactoring

--- src/main/java/it.polimi.ingsw.model/InvalidWarehouseInsertionException.java
+++ src/main/java/it.polimi.ingsw.model/InvalidWarehouseInsertionException.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.model;
 /**
- *Exception called when the insertion doesn't resoect the warehouse rules, or the cell was already full.
+ *Exception called when the insertion doesn't respect the warehouse rules, or the cell was already full.
  */
 public class InvalidWarehouseInsertionException extends Exception{
-    //the insertion doesn't respect the warehouse rules, or the cell was already full
 }
