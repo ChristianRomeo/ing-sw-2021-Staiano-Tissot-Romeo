8e847b9b00dcb702c3dcad780fcf84419dfdf26c
Tommaso Tissot <tissot.tommaso@outlook.it>
2021-05-25 12:54:04 +0200

    added other model tests

--- src/test/java/it/polimi/ingswModelTests/GameTest.java
+++ src/test/java/it/polimi/ingswModelTests/GameTest.java
@@ -2,7 +2,10 @@ package it.polimi.ingswModelTests;
 import it.polimi.ingsw.controller.*;
 import it.polimi.ingsw.model.*;
 
+
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 /**
  * Unit tests.
@@ -12,4 +15,65 @@ public class GameTest {
     public void initGameTest(){
 
     }
+
+    @Test
+    public void getAndSetWantedNumPlayersTest() throws IOException {
+        Game game = new Game();
+        boolean check;
+        game.setWantedNumPlayers(3);
+        check = game.getWantedNumPlayers() == 1;
+        assert(!check);
+        check = game.getWantedNumPlayers() == 3;
+        assert(check);
+    }
+
+    @Test
+    public void getPlayersTest() throws IOException {
+        Game game = new Game();
+        Player player1 = new Player("tom");
+        Player player2 = new Player("chris");
+        Player player3 = new Player("enrico");
+        game.addNewPlayer(player1);
+        game.addNewPlayer(player2);
+        game.addNewPlayer(player3);
+        assert(game.getPlayers().size() == 3);
+        assert(game.getPlayers().get(0).getNickname().equals("tom"));
+        assert(game.getPlayers().get(1).getNickname().equals("chris"));
+        assert(game.getPlayers().get(2).getNickname().equals("enrico"));
+    }
+
+    @Test
+    public void getPlayersByNicknameTest() throws IOException {
+        Game game = new Game();
+        Player player1 = new Player("tom");
+        boolean check;
+        game.addNewPlayer(player1);
+        check = game.getPlayerByNickname("nick") != null;
+        assert(!check);
+        check = game.getPlayerByNickname("tom") != null;
+        assert(check);
+    }
+
+    @Test
+    public void getPlayerByIndexTest() throws IOException {
+        Game game = new Game();
+        Player player1 = new Player("tom");
+        Player player2 = new Player("chris");
+        Player player3 = new Player("enrico");
+        game.addNewPlayer(player1);
+        game.addNewPlayer(player2);
+        game.addNewPlayer(player3);
+        assert(game.getPlayerByIndex(0).getNickname().equals(player1.getNickname()));
+        assert(game.getPlayerByIndex(1).getNickname().equals(player2.getNickname()));
+        assert(game.getPlayerByIndex(2).getNickname().equals(player3.getNickname()));
+    }
+
+    @Test
+    public void isActiveTest() throws IOException {
+        Game game = new Game();
+        assert(game.isActive());
+        game.setInactive();
+        assert(!game.isActive());
+    }
 }
+
---
+++ src/test/java/it/polimi/ingswModelTests/SameTypeTripleTest.java
@@ -0,0 +1,37 @@
+package it.polimi.ingswModelTests;
+import it.polimi.ingsw.model.Resource;
+import it.polimi.ingsw.model.SameTypeTriple;
+import org.junit.jupiter.api.Test;
+
+public class SameTypeTripleTest {
+
+    @Test
+    public void getAndSetVal1Test() {
+        SameTypeTriple sameTypeTriple = new SameTypeTriple();
+        sameTypeTriple.setVal1(Resource.COIN);
+        assert(!sameTypeTriple.getVal1().equals(Resource.SERVANT));
+        assert(!sameTypeTriple.getVal1().equals(Resource.SHIELD));
+        assert(!sameTypeTriple.getVal1().equals(Resource.STONE));
+        assert(sameTypeTriple.getVal1().equals(Resource.COIN));
+    }
+
+    @Test
+    public void getAndSetVal2Test() {
+        SameTypeTriple sameTypeTriple = new SameTypeTriple();
+        sameTypeTriple.setVal2(Resource.COIN);
+        assert(!sameTypeTriple.getVal2().equals(Resource.SERVANT));
+        assert(!sameTypeTriple.getVal2().equals(Resource.SHIELD));
+        assert(!sameTypeTriple.getVal2().equals(Resource.STONE));
+        assert(sameTypeTriple.getVal2().equals(Resource.COIN));
+    }
+
+    @Test
+    public void getAndSetVal3Test() {
+        SameTypeTriple sameTypeTriple = new SameTypeTriple();
+        sameTypeTriple.setVal3(Resource.COIN);
+        assert(!sameTypeTriple.getVal3().equals(Resource.SERVANT));
+        assert(!sameTypeTriple.getVal3().equals(Resource.SHIELD));
+        assert(!sameTypeTriple.getVal3().equals(Resource.STONE));
+        assert(sameTypeTriple.getVal3().equals(Resource.COIN));
+    }
+}
